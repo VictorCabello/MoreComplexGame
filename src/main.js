22027cc6c687b0261808156f4907b23caf0a42ce
Victor Cabello <vmeca87@gmail.com>
2021-03-22 00:33:08 -0600

    First tidle integration

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Phaser from 'phaser';
+import map from './assets/tilde/level1.json';
+import tiles from 'url:./assets/img/generic_platformer_tiles.png';
 
 var config = {
     type: Phaser.AUTO,
     width: 800,
-    height: 600,
+    height: 640,
     physics: {
         default: 'arcade',
         arcade: {
@@ -20,30 +22,14 @@ var game = new Phaser.Game(config);
 
 function preload ()
 {
-    this.load.setBaseURL('http://labs.phaser.io');
-
-    this.load.image('sky', 'assets/skies/space3.png');
-    this.load.image('logo', 'assets/sprites/phaser3-logo.png');
-    this.load.image('red', 'assets/particles/red.png');
+    this.load.image('tiles', tiles);
+    this.load.tilemapTiledJSON('map', map);
 }
 
-function create ()
-{
-    this.add.image(400, 300, 'sky');
+function create () {
+    const map = this.make.tilemap({ key: 'map' });
+    const tileset = map.addTilesetImage('generic', 'tiles');
 
-    var particles = this.add.particles('red');
-
-    var emitter = particles.createEmitter({
-        speed: 100,
-        scale: { start: 1, end: 0 },
-        blendMode: 'ADD'
-    });
-
-    var logo = this.physics.add.image(400, 100, 'logo');
-
-    logo.setVelocity(100, 200);
-    logo.setBounce(1, 1);
-    logo.setCollideWorldBounds(true);
-
-    emitter.startFollow(logo);
+    const background = map.createLayer('background', tileset, 0, 0);
+    const palftom = map.createLayer('platform', tileset, 0, 0);
 }
