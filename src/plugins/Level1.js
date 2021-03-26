4d6bcf2c6ac9bef4a9d119a144bae3d42964d03d
Victor Cabello <vmeca87@gmail.com>
2021-03-26 17:34:50 -0600

    refactor (#5)

--- src/plugins/Level1.js
+++ src/plugins/Level1.js
@@ -6,78 +6,42 @@
 import Phaser from 'phaser';
 import map from '../assets/tilde/level1.json';
 import tiles from 'url:../assets/img/generic_platformer_tiles.png';
-/**
- * Plugin to control the map of the level 1.
- *
- * @class
- * @extends Phaser.Plugins.ScenePlugin
- * @name MoreComplexGame.MapLevel01Plugin
- */
+
 export default class MapLevel01Plugin extends Phaser.Plugins.ScenePlugin {
 
-    /**
-     * Plugin that handles all the player animations
-     *
-     * @name platforms
-     * @type {Phaser.Tilemaps.TilemapLayer}}
-     */
     platforms = null;
 
-    /**
-     * This function load the main assets for the level01, which include
-     * the following:
-     *
-     * 1) tiles: A PNG image that contians genaral patterns that are used on the level.
-     * 2) map: The map of this level created with Tiled.
-     *
-     * @see Phaser.Plugins.ScenePlugin.boot()
-     * @see https://www.mapeditor.org
-     * @see MoreComplexGame.PlayerPlugin
-     */
     boot() {
         this.scene.load.image('tiles', tiles);
         this.scene.load.tilemapTiledJSON('map', map);
     }
 
-    /**
-     * Initialize the map and return the platforms that could interact with the player.
-     *
-     * A tileset called "generic" should exit in the tile map.
-     *
-     * @return {Phaser.Tilemaps.TilemapLayer} platforms
-     */
     create() {
         if(this.platforms === null){
-            const map = this.scene.make.tilemap({ key: 'map' });
-            const tileset = map.addTilesetImage('generic', 'tiles');
+            this.map = this.scene.make.tilemap({ key: 'map' });
+            const tileset = this.map.addTilesetImage('generic', 'tiles');
 
-            this.createBackground(map, tileset);
-            this.platforms = this.createPlaforms(map, tileset)
+            this.createBackground(tileset);
+            this.platforms = this.createPlaforms(tileset);
         }
 
         return this.platforms;
     }
 
-    /**
-     * Create the background.
-     *
-     * A layer called "background" should exit in the tile map.
-     * @private
-     */
-    createBackground(map, tileset) {
-        map.createLayer('background', tileset, 0, 0);
+    createBackground(tileset) {
+        this.map.createLayer('background', tileset, 0, 0);
     }
 
-    /**
-     * Create the platform.
-     *
-     * A layer called "platform" should exit in the tile map.
-     * @private
-     * @return {Phaser.Tilemaps.TilemapLayer} platforms
-     */
-    createPlaforms(map, tileset) {
-        const platforms = map.createLayer('platform', tileset, 0, 0);
+    createPlaforms(tileset) {
+        const platforms = this.map.createLayer('platform', tileset, 0, 0);
         platforms.setCollisionByExclusion(-1, true);
         return platforms;
+    }
+
+    getGameObject(name){
+        const layer = this.map.getObjectLayer('GameObjects');
+        const wanted = layer.objects.find((element) => element.name === name);
+        if(wanted) { return wanted; }
+        else {throw  name + ' not found in GameObjects'}
     }
 }
